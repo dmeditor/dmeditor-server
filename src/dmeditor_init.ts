794c0890f959a47087419caad64892e8106e7afb
Chen Xiongjie <chen.xcmail@gmail.com>
2023-11-10 19:54:21 +0100

    updated: init

--- src/dmeditor_init.ts
+++ src/dmeditor_init.ts
@@ -1,7 +1,6 @@
 
 import { registerTool, registerTemplate } from 'dmeditor';
 import {toolFeedbackForm, toolCarousel, toolEmbedContent, toolContentGallery, toolContentGrid} from 'dmeditor-digimaker';
-import Init from "dmeditor";
 
 registerTool(toolFeedbackForm);
 registerTool(toolContentGrid);
